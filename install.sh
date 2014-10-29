f128585ea9a34fd40682e1c88fa83ac8345ae343
Brian Hines <brian@projectweekend.net>
2014-10-29 05:54:46 -0500

    Changes to install script

--- install.sh
+++ install.sh
@@ -1,9 +1,18 @@
 #!/usr/bin/env bash
 
+# Install
 apt-get update
 apt-get -y upgrade
 apt-get -y --force-yes install upstart
+pip install -r requirements.txt
 
+# Upstart
 cp ./install/pi-box.conf /etc/init
 
-pip install -r requirements.txt
+# Move program
+mkdir /opt/Pi-Box
+cp ./main.py /opt/Pi-Box
+
+# Clean up
+rm -r /home/pi/Pi-Box
+mkdir /home/pi/Pi-Box
