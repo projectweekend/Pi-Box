67e25fd31297329de1de98a6e74fea11b6aadffd
Brian Hines <brian@projectweekend.net>
2014-10-30 17:44:34 -0500

    Changing the install script

--- install.sh
+++ install.sh
@@ -6,11 +6,9 @@ apt-get -y upgrade
 apt-get -y --force-yes install upstart
 pip install -r requirements.txt
 
-# Upstart
-cp ./install/pi-box.conf /etc/init
-
 # Move program
 mkdir /opt/Pi-Box
 cp ./main.py /opt/Pi-Box
 
-# Prompt user for a path here...save it in upstart script
+# Setup
+python ./install/setup_pi_box.py
---
+++ install/setup_pi_box.py
@@ -0,0 +1,14 @@
+import os
+
+
+print("Example Pi Box path: /home/username/my-pi-box")
+pi_box_directory = raw_input("Pi Box path: ")
+
+if not os.path.isdir(pi_box_directory):
+	os.makedirs(pi_box_directory)
+
+with open('pi-box-conf-template.txt', 'r') as f:
+	upstart_template = f.read()
+
+with open('/etc/init/pi-box.conf', 'w+') as f:
+	f.write(upstart_template.format(pi_box_directory))
